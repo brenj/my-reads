614a1e4d1cf144a15d235dd8b3dad26a470b01e4
brenj <brenj@mailbox.org>
2017-09-23 18:10:15 -0500

    Remove 404 handling to test gh-pages hack

--- src/App.js
+++ src/App.js
@@ -106,7 +106,6 @@ class App extends React.Component {
               </div>
             )}
           />
-          <Route component={PageNotFound} />
         </Switch>
         <Footer />
       </div>
