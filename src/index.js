eff1d2cccb0eaafb93cc9fab3b9fa6124f013272
brenj <brenj@mailbox.org>
2017-09-26 08:07:35 -0500

    Use HashRouter to work with Github Pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 
 import './index.css';
@@ -8,7 +8,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <BrowserRouter><App /></BrowserRouter>,
+  <HashRouter><App /></HashRouter>,
   document.getElementById('root'),
 );
 registerServiceWorker();
